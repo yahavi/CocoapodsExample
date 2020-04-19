1015fffcd8681e8f49a753ca9a7f4bee59a71204
yahavi <yahavi@jfrog.com>
2020-04-19 08:43:00 +0300

    Initial commit

---
+++ Example/CocoapodsExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CocoapodsExample
+//
+//  Created by yahavi on 04/19/2020.
+//  Copyright (c) 2020 yahavi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
